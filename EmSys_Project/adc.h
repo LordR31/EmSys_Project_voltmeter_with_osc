9d9955ed511ec07177d76982b250f90cc678c662
Andrei Visoiu <visoiuandre@gmail.com>
2025-10-27 16:52:10 +0200

    feat: added a function for ADC Toggle

--- EmSys_Project/adc.h
+++ EmSys_Project/adc.h
@@ -8,14 +8,17 @@
 #define ADC_MAX_VALUE 1023 // 10 bit adc -> 0 - 1023 range
 #define NUM_SAMPLES 10
 
-#define ADC_STEP_LOW 4.760 / 1023.0
-#define ADC_GAIN_LOW 1.05
-#define ADC_OFFSET_LOW 0.009
+#define ADC_STEP_LOW 5.018 / 1023.0
+//#define ADC_GAIN_LOW 1.05
+#define ADC_GAIN_LOW 1
+//#define ADC_OFFSET_LOW 0.009
+#define ADC_OFFSET_LOW 0.000
 
 #define ADC_STEP_HIGH 0.024
 #define ADC_GAIN_HIGH 1
 #define ADC_OFFSET_HIGH 0.024
 
 void ADC_Init(void);
+void Toggle_ADC();
 float ADC_measure(bool is_high_voltage);
 #endif /* ADC_H_ */
